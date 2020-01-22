bafccd1f6c1a0d5e7416011e81f1ee1458bb9b02
donneh4 <donny@i-aspect.nl>
2020-01-22 15:58:02 +0100

    Add UiController and gui.fxml

--- src/main/java/define/App.java
+++ src/main/java/define/App.java
@@ -1,14 +1,31 @@
 package define;
 
 import define.application.serviceinterfaces.Controller;
-public class App {
+import define.ui.UiController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class App extends Application {
     
 	public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
-        
-        Controller controller = new Controller();
-        controller.defineAttributeRangeRule(1, "oracle", "persons", "age", "CHK_rangeRuleTest", 10, 20);
-        
+	    launch(args);
     }
-	
+
+    public void start(Stage primaryStage) throws Exception {
+	    UiController controller = new UiController();
+        FXMLLoader loader = new FXMLLoader();
+        loader.setController(controller);
+        loader.setLocation(new URL("file:///c:/laragon/www/TOSAD2020-define-business-rule/gui.fxml"));
+        VBox vbox = loader.<VBox>load();
+        Scene scene = new Scene(vbox);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        System.out.println(controller);
+    }
 }
